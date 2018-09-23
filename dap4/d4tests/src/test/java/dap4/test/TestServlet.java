4c32a5c7301d184cff3d9a2b0a7798ae0513ea91
Dennis Heimbigner <dmh@ucar.edu>
2018-09-23 14:36:55 -0600

    ckp

--- dap4/d4tests/src/test/java/dap4/test/TestServlet.java
+++ dap4/d4tests/src/test/java/dap4/test/TestServlet.java
@@ -25,11 +25,11 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
-import thredds.mock.web.MockTdsContextLoader;
-import thredds.mock.web.TdsResourceBasePath;
 import thredds.server.dap4.Dap4Controller;
 import ucar.nc2.jni.netcdf.Nc4Iosp;
 import ucar.nc2.jni.netcdf.Nc4wrapper;
+import ucar.unidata.util.test.mock.MockTdsContextLoader;
+import ucar.unidata.util.test.mock.TdsResourceBasePath;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -67,7 +67,7 @@ USESPRING to remind me of what needs to be done someday.
         locations = {"/applicationContext.xml", "/spring-servlet.xml"}
         ,loader = MockTdsContextLoader.class
         )
-@TdsResourceBasePath(path="file:d:/git/thredds/dap4\\d4tests/src/main/webapp")
+@TdsResourceBasePath(path="file:d:/git/thredds/dap4/d4tests/src/main/webapp")
 /**/
 
 public class TestServlet extends DapTestCommon
